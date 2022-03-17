90f8163e8b64c96beec8bdb18273ce9450982108
SukranSirinov <100930440+SukranSirinov@users.noreply.github.com>
2022-03-17 11:57:29 +0400

    Add files via upload

---
+++ ConsoleApp5/Group.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    internal class Group
+    {
+        private string _no;
+        private int _studentlimit;
+        public string No
+        {
+            get
+            {
+                return this._no; 
+            }
+            set
+            {
+                if(value.Length > 0 && value.Length<5)
+                    this._no = value;
+            }
+        }
+        public int Studentlimit
+        {
+            get
+            {
+                return this._studentlimit;
+            }
+            set
+            {
+                if(value>0)
+                    this._studentlimit = value;
+            }
+        }
+        static bool CheckNo(string str)
+        {
+            bool hasUpper= false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(char.IsUpper(str[0]) && char.IsUpper(str[1]))
+                    hasUpper = true;    
+
+            }
+            return hasUpper;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string no;
+            do
+            {
+                Console.WriteLine("No daxil edin:");
+                no = Console.ReadLine();
+
+            } while (string.IsNullOrWhiteSpace(no) || no.Length >0 || no.Length <6);
+
+
+            int studentlimit;
+            do
+            {
+                Console.WriteLine("Studentlimit daxil edin:");
+                studentlimit = Console.ReadLine();
+
+            } while (!CheckGroup(studentlimit));
+
+            User user = new User(no, studentlimit);
+
+            Console.WriteLine($"No: {user.No} \nStudentlimit: {user.Studentlimit}");
+        }
+        static bool CheckNo(string str)
+        {
+            bool hasUpper = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[0]) && char.IsUpper(str[1]))
+                    hasUpper = true;
+
+            }
+            return hasUpper;
+        }
+    }
+    
+}
